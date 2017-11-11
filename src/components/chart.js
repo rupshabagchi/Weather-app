11ba0f4c06c8429155f1d08d7ee3b381dce57d67
Rupsha <rupsha.bagchi@gmail.com>
2017-11-11 12:28:29 +0200

    adding charts as a component

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
     return(
       <div>
         <Sparklines height={100} width={150} data={props.data}>
           <SparklinesLine color = {props.color} />
+          <SparklinesReferenceLine type="avg" />
         </Sparklines>
       </div>
     );
