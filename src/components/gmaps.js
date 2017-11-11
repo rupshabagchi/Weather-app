6cb1b3b0c2329b4378e5ea3da6eb5b5066ceb448
Rupsha <rupsha.bagchi@gmail.com>
2017-11-11 12:49:48 +0200

    adding google maps

--- src/components/gmaps.js
+++ src/components/gmaps.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class GMap extends Component {
   componentDidMount(){
-    new google.maps.Map(this.refs.map, {});
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
   }
 
   render(){
