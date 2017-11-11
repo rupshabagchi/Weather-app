9ed4c50c834499ef263fe5b39cc376ac84235fc6
Rupsha <rupsha.bagchi@gmail.com>
2017-11-11 12:25:44 +0200

    adding charts

--- src/containers/list.js
+++ src/containers/list.js
@@ -1,14 +1,27 @@
 import React,{ Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class List extends Component {
 
-  renderWeather(event) {
-    const name = event.city.name;
+  renderWeather(data) {
+    const name = data.city.name;
+    console.log(data.list[0])
+    const temp_list = data.list.map(weather => weather.main.temp );
+    const pressure_list = data.list.map(weather => weather.main.pressure );
+    const humidity_list = data.list.map(weather => weather.main.humidity );
+
     return(
         <tr key={name}>
+          <td> {name} </td>
           <td>
-            {name}
+            <Chart data={temp_list} color="orange" />
+          </td>
+          <td>
+            <Chart data={pressure_list} color="black" />
+          </td>
+          <td>
+            <Chart data={humidity_list} color="blue" />
           </td>
         </tr>
     );
